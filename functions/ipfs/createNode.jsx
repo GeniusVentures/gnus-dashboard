43667660b5d1d09ade798d3ec3c7f9db57d9a9a9
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-04-11 13:00:30 -0400

    ipfs testing

--- functions/ipfs/createNode.jsx
+++ functions/ipfs/createNode.jsx
@@ -20,19 +20,43 @@ import { MemoryDatastore } from "datastore-core";
 // import { PeerId } from "@libp2p/interface";
 import { DNS } from "@multiformats/dns";
 import { plaintext } from "@libp2p/plaintext";
-import { createEd25519PeerId } from "@libp2p/peer-id-factory";
+import {
+	createEd25519PeerId,
+	createFromPrivKey,
+	createFromPubKey,
+} from "@libp2p/peer-id-factory";
+import { peerIdFromKeys } from "@libp2p/peer-id";
+import { generateKeyPair } from "@libp2p/crypto/keys";
 
 let node = null;
 
 const createNode = async () => {
+	let id = null;
+	id = await generateKeyPair("Ed25519");
+	console.log(id);
 	try {
 		const blockstore = new MemoryBlockstore();
 		const datastore = new MemoryDatastore();
 
 		const libp2p = await create({
+			// peerId: await peerIdFromKeys(
+			// 	new Uint8Array([
+			// 		53, 91, 255, 177, 92, 238, 131, 20, 154, 5, 81, 183, 7, 155, 121, 66,
+			// 		239, 88, 19, 76, 65, 239, 71, 62, 13, 180, 136, 78, 135, 197, 198,
+			// 		184, 247, 10, 55, 188, 245, 75, 169, 223, 220, 89, 43, 38, 241, 178,
+			// 		242, 15, 68, 117, 101, 182, 48, 188, 147, 177, 191, 179, 163, 67, 1,
+			// 		181, 83, 64,
+			// 	]),
+			// 	new Uint8Array([
+			// 		243, 112, 72, 250, 157, 212, 64, 22, 121, 150, 70, 135, 172, 32, 91,
+			// 		13, 134, 228, 101, 189, 123, 12, 131, 150, 35, 210, 184, 3, 118, 200,
+			// 		135, 156,
+			// 	]),
+			// ),
+			peerId: await createEd25519PeerId(),
 			dns: DNS,
 			addresses: {
-				listen: ["/ip4/10.14.0.2/tcp/52428"],
+				listen: ["/ip4/10.14.0.2/tcp/52453"],
 			},
 			transports: [
 				tcp(),
