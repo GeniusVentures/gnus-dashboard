d16509cdca57433a56d541d6d0d2ab974f4d2bb4
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-04-11 19:10:02 -0400

    ipfs testing

--- functions/ipfs/createNode.jsx
+++ functions/ipfs/createNode.jsx
@@ -17,43 +17,71 @@ import { bootstrapConfig } from "data/ipfs/bootstrapConfig";
 import { createLibp2p as create } from "libp2p";
 import { MemoryBlockstore } from "blockstore-core";
 import { MemoryDatastore } from "datastore-core";
-// import { PeerId } from "@libp2p/interface";
-import { DNS } from "@multiformats/dns";
-import { plaintext } from "@libp2p/plaintext";
+//import { PeerId, Ed25519PeerId } from "@libp2p/interface";
+import { PublicKey, PrivateKey } from "@libp2p/interface";
+import { peerIdFromString } from "@libp2p/peer-id";
 import {
 	createEd25519PeerId,
 	createFromPrivKey,
-	createFromPubKey,
 } from "@libp2p/peer-id-factory";
-import { peerIdFromKeys } from "@libp2p/peer-id";
-import { generateKeyPair } from "@libp2p/crypto/keys";
+import { DNS } from "@multiformats/dns";
+import { plaintext } from "@libp2p/plaintext";
+import {
+	generateKeyPair,
+	marshalPrivateKey,
+	unmarshalPrivateKey,
+	marshalPublicKey,
+	unmarshalPublicKey,
+} from "@libp2p/crypto/keys";
+import { peerIdFromKeys, peerIdFromBytes } from "@libp2p/peer-id";
+import { multiaddr } from "@multiformats/multiaddr";
+import { Ed25519PrivateKey } from "@libp2p/crypto/keys";
 
 let node = null;
 
 const createNode = async () => {
-	// let id = null;
-	// id = await generateKeyPair("Ed25519");
-	// console.log(id);
 	try {
 		const blockstore = new MemoryBlockstore();
 		const datastore = new MemoryDatastore();
+		//const keyPair = await createEd25519PeerId();
+		//const myEd25519PeerId = keyPair;
+		const publicKeyBytes = new Uint8Array([
+			241, 63, 188, 197, 210, 57, 246, 51, 214, 56, 128, 62, 140, 200, 94, 154,
+			254, 154, 114, 80, 146, 242, 148, 101, 69, 160, 153, 3, 206, 171, 137,
+			145,
+		]);
+		const privateKeyBytes = new Uint8Array([
+			160, 235, 133, 48, 113, 183, 229, 39, 252, 154, 199, 184, 21, 171, 179,
+			88, 137, 11, 81, 55, 16, 123, 228, 124, 58, 10, 12, 94, 153, 101, 34, 122,
+			241, 63, 188, 197, 210, 57, 246, 51, 214, 56, 128, 62, 140, 200, 94, 154,
+			254, 154, 114, 80, 146, 242, 148, 101, 69, 160, 153, 3, 206, 171, 137,
+			145,
+		]);
+		const privateKey = new Ed25519PrivateKey(privateKeyBytes, publicKeyBytes);
+		//const key = await generateKeyPair('Ed25519', 1024);
+		//const key = new PrivateKey();
 
+		const myEd25519PeerId = await createFromPrivKey(privateKey);
+		const mydirectPeers = [
+			{
+				id: peerIdFromString(
+					"12D3KooWP49mSuMJ3Z4VARZM5av5cxbHFAmd7kVk31XvyGjcVi8q",
+				),
+				addrs: [multiaddr("/ip4/192.168.46.18/tcp/40001")],
+			},
+			{
+				id: peerIdFromString(
+					"12D3KooWN4QE8uaE5EAJFXBduYaRaBDYkxNbCJMvxqT5H2gU6hhG",
+				),
+				addrs: [multiaddr("/ip4/192.168.46.18/tcp/40002")],
+			},
+		];
+		const opubptions = {
+			emitSelf: true,
+			directPeers: mydirectPeers,
+		};
 		const libp2p = await create({
-			// peerId: await peerIdFromKeys(
-			// 	new Uint8Array([
-			// 		53, 91, 255, 177, 92, 238, 131, 20, 154, 5, 81, 183, 7, 155, 121, 66,
-			// 		239, 88, 19, 76, 65, 239, 71, 62, 13, 180, 136, 78, 135, 197, 198,
-			// 		184, 247, 10, 55, 188, 245, 75, 169, 223, 220, 89, 43, 38, 241, 178,
-			// 		242, 15, 68, 117, 101, 182, 48, 188, 147, 177, 191, 179, 163, 67, 1,
-			// 		181, 83, 64,
-			// 	]),
-			// 	new Uint8Array([
-			// 		243, 112, 72, 250, 157, 212, 64, 22, 121, 150, 70, 135, 172, 32, 91,
-			// 		13, 134, 228, 101, 189, 123, 12, 131, 150, 35, 210, 184, 3, 118, 200,
-			// 		135, 156,
-			// 	]),
-			// ),
-			peerId: await createEd25519PeerId(),
+			peerId: myEd25519PeerId,
 			dns: DNS,
 			addresses: {
 				listen: ["/ip4/10.14.0.2/tcp/52453"],
@@ -66,10 +94,10 @@ const createNode = async () => {
 				// }),
 			],
 			connectionEncryption: [noise(), plaintext()],
-			streamMuxers: [mplex()],
+			streamMuxers: [yamux(), mplex()],
 			peerDiscovery: [bootstrap(bootstrapConfig)],
 			services: {
-				pubsub: gossipsub(),
+				pubsub: gossipsub(opubptions),
 				dht: kadDHT({
 					clientMode: true,
 					validators: {
@@ -81,6 +109,7 @@ const createNode = async () => {
 				}),
 				identify: identify(),
 				keychain: keychain(KeychainInit),
+				ping: ping(),
 				// ping: ping([
 				// 	"/ip4/174.105.208.56/tcp/40001/p2p/12D3KooWP49mSuMJ3Z4VARZM5av5cxbHFAmd7kVk31XvyGjcVi8q",
 				// 	"/ip4/174.105.208.56/tcp/40002/p2p/12D3KooWN4QE8uaE5EAJFXBduYaRaBDYkxNbCJMvxqT5H2gU6hhG",
@@ -98,23 +127,35 @@ const createNode = async () => {
 		});
 		node = heliaNode;
 		console.log("line74: " + node.libp2p.getMultiaddrs()[0]);
-		libp2p.services.pubsub.subscribe("SuperGenius");
-
-		node.libp2p.addEventListener("connection:open", () => {
+		console.log("wew " + node.libp2p.peerId.toString());
+		console.log("public key " + node.libp2p.peerId.publicKey);
+		console.log("privat key " + node.libp2p.peerId.privateKey);
+		libp2p.addEventListener("connection:open", () => {
 			console.log("opened" + node.libp2p.getPeers().length);
 		});
-		node.libp2p.addEventListener("connection:close", () => {
+		libp2p.addEventListener("connection:close", () => {
 			console.log("closed" + node.libp2p.getPeers().length);
 		});
 
-		node.libp2p.services.pubsub.addEventListener("message", (event) => {
-			const topic = event.detail.topic;
-			const message = toString(event.detail.data);
-
-			console.log(topic + `\n` + message);
+		libp2p.services.pubsub.addEventListener("message", (message) => {
+			console.log(
+				`${message.detail.topic}:`,
+				new TextDecoder().decode(message.detail.data),
+			);
 		});
+		libp2p.services.pubsub.subscribe("SuperGenius");
+
+		libp2p.services.pubsub.publish(
+			"SuperGenius",
+			new TextEncoder().encode("banana"),
+		);
+
 		setInterval(() => {
 			const peerList = libp2p.services.pubsub.getSubscribers();
+			libp2p.services.pubsub.publish(
+				"SuperGenius",
+				new TextEncoder().encode("banana"),
+			);
 			console.log(peerList);
 		}, 3000);
 	} catch (err) {
