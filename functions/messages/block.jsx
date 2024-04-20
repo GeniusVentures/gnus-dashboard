09d3f9e95022b913d9e7499829c76d3bc522498d
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-04-20 12:46:37 -0400

    Update block.jsx

--- functions/messages/block.jsx
+++ functions/messages/block.jsx
@@ -11,7 +11,7 @@ const blockMsg = (block) => {
 
 		const newBlock = {
 			block: 1,
-			hash: "hash",
+			hash: String.fromCharCode(...mint.dagStruct.parentHash),
 			transactions: 1, // Currently transaction count is always 1.
 			time: timestamp,
 		};
