81ac2a3492c6db967ad730481d35051e9db63766
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-04-20 14:12:12 -0400

    pubsub testing

--- functions/messages/block.jsx
+++ functions/messages/block.jsx
@@ -4,18 +4,18 @@ let keys = [];
 
 const blockMsg = (block, key) => {
 	try {
-		// Get current time in milliseconds
-		const currentTimeMillis = Date.now();
-		// Get the last three digits from performance.now() for microsecond precision
-		const microsecondPart = performance.now().toFixed(3).split(".")[1];
-		// Combine milliseconds and microseconds
-		const timestamp = `${currentTimeMillis}${microsecondPart}`;
+		// // Get current time in milliseconds
+		// const currentTimeMillis = Date.now();
+		// // Get the last three digits from performance.now() for microsecond precision
+		// const microsecondPart = performance.now().toFixed(3).split(".")[1];
+		// // Combine milliseconds and microseconds
+		// const timestamp = `${currentTimeMillis}${microsecondPart}`;
 		if (!keys.includes(key)) {
 			const newBlock = {
 				block: key.split("/")[3],
 				hash: String.fromCharCode(...block.parentHash),
 				transactions: 1, // Currently transaction count is always 1.
-				time: timestamp,
+				time: Date.now(),
 			};
 
 			updateBCD([newBlock, ...blockchainData].slice(0, 200));
