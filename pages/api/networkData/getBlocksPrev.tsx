08208f93b75a6874900aef80fe71af9760ad629e
Jesse Wachtel <84250138+blockchainTrucker@users.noreply.github.com>
2024-05-24 16:29:07 -0400

    removed console.logs

--- pages/api/networkData/getBlocksPrev.tsx
+++ pages/api/networkData/getBlocksPrev.tsx
@@ -1,8 +1,7 @@
 import { blockchainData } from "data/prepared/blockchainInfo";
 
 const getTransPrev = (req, res) => {
-	console.log(blockchainData);
-	res.status(200).json(blockchainData);
+  res.status(200).json(blockchainData);
 };
 
 export default getTransPrev;
--- pages/api/networkData/getTransPrev.tsx
+++ pages/api/networkData/getTransPrev.tsx
@@ -1,8 +1,7 @@
 import { transactionData } from "data/prepared/transactionInfo";
 
 const getTransPrev = (req, res) => {
-	console.log(transactionData);
-	res.status(200).json(transactionData);
+  res.status(200).json(transactionData);
 };
 
 export default getTransPrev;
